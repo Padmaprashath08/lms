dd48c69825fb74d44c9c42028b66e9356f66299e
Padmaprashath <padmaprashath.s2023ai-ds@sece.ac.in>
2025-08-05 21:29:10 +0530

    Update api.js

--- project/src/services/api.js
+++ project/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://lms-sbo4.onrender.com//api';
+const API_BASE_URL = 'https://lms-sbo4.onrender.com/api';
 
 // Create axios instance with default config
 const api = axios.create({
