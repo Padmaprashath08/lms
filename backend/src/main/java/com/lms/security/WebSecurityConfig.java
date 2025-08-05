69bfe05c22e458d3de5cd3ab5251d4a3573c3da7
Padmaprashath <padmaprashath.s2023ai-ds@sece.ac.in>
2025-08-05 21:46:09 +0530

    Update WebSecurityConfig.java

--- backend/src/main/java/com/lms/security/WebSecurityConfig.java
+++ backend/src/main/java/com/lms/security/WebSecurityConfig.java
@@ -61,7 +61,7 @@ public class WebSecurityConfig {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/","/api/auth/**").permitAll()
                 .requestMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated();
 
